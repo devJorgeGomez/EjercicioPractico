3134a49791dfb6a0e7bde50751b1a8e853422657
devJorgeGomez <jorgegomez98125@gmail.com>
2023-08-08 19:59:17 -0500

    first commit

---
+++ src/main/java/com/example/demo/NumerosretoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumerosretoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumerosretoApplication.class, args);
+	}
+
+}
